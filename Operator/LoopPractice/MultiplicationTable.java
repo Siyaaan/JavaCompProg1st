4c3bc88501aa1abf585003294d28cea2b69a29d8
Siyaaan <Seanortegapulga13@gmail.com>
2024-09-20 20:59:44 +0800

    updated multiplication table

--- Operator/LoopPractice/MultiplicationTable.java
+++ Operator/LoopPractice/MultiplicationTable.java
@@ -6,7 +6,7 @@ public class MultiplicationTable {
         Scanner get = new Scanner(System.in);
         int num = get.nextInt();
 
-        for (int i=10; i>=1; i--){
+        for (int i=10; i>=1; i--){//to reverse this to normal; switch the number of initialization and condition and make the condition less than the number. Make the update positive after
             int product = i * num;
 
             System.out.println(num + " x " + i + " = " + product);
