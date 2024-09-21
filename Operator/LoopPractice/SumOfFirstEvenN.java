b79f47e16d50fb3d1cb643c4348ed93ed42d3409
Siyaaan <Seanortegapulga13@gmail.com>
2024-09-21 15:58:30 +0800

    factorial addedd

--- Operator/LoopPractice/SumOfFirstEvenN.java
+++ Operator/LoopPractice/SumOfFirstEvenN.java
@@ -2,7 +2,7 @@ package Operator.LoopPractice;
 
 import java.util.Scanner;
 
-public class ProductOfFirstEvenN {
+public class SumOfFirstEvenN {
     public static void main(String[] args) {
         Scanner get = new Scanner(System.in);
 
--- Operator/LoopPractice/SumOfOddNum.java
+++ Operator/LoopPractice/SumOfOddNum.java
@@ -5,6 +5,19 @@ public class SumOfOddNum {
     public static void main(String[] args) {
         Scanner get = new Scanner(System.in);
 
+        int odd = get.nextInt();
+        int num = 0;
+        int oddNum = 1;
+
+        for (int i = 1; i <= odd; i++) {
+            num = num + oddNum;
+            oddNum = oddNum + 2;
+
+                     
+        }
+        System.out.println(num);
+       
+        get.close();
         
     }
 }
