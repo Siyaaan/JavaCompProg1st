5ee1a8a8adcdbfc405d3ab1959897c38ddd81d7e
Siyaaan <Seanortegapulga13@gmail.com>
2024-09-21 16:21:51 +0800

    practiced reverse

--- Operator/LoopPractice/Reverse.java
+++ Operator/LoopPractice/Reverse.java
@@ -1,23 +1,12 @@
 package Operator.LoopPractice;
 import java.util.Scanner;
 
-public class Reverse {
+public class Reverse{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        System.out.print("Enter a number: ");
-        int num = sc.nextInt();  // Take user input
-        int reversedNum = 0;
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
 
-        sc.close();
-        
-        for(; num > 0; num /= 10) {   // Continue until num becomes 0
-            int lastDigit = num % 10; // Extract last digit
-            reversedNum = reversedNum * 10 + lastDigit; // Build reversed number
-        }
-            System.out.println("Reversed Number: " + reversedNum);
+        for (int i = 1; i < )
 
-        
-        // For loop to reverse the number     
     }
 }
